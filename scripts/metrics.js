ec99511751e6cdf33b6aa391b96429f1d025d9cf
Guilherme Rogelin Vial <78435405+GRVial@users.noreply.github.com>
2024-07-12 17:28:10 -0300

    Update metrics.js

--- scripts/metrics.js
+++ scripts/metrics.js
@@ -1,21 +1,19 @@
-const REPO = '2023.2-PrintGo-FrontEnd'
+const REPO = '2024.1-PrintGo-FrontEnd'
 const OWNER = 'fga-eps-mds'
-const SONAR_ID = 'fga-eps-mds_2023.2-PrintGo-FrontEnd'
+const SONAR_ID = 'fga-eps-mds_2024.1-PrintGo-FrontEnd'
 
-const METRIC_LIST = [
-    'files',
-    'functions',
-    'complexity',
-    'comment_lines_density',
-    'duplicated_lines_density',
-    'coverage',
-    'ncloc',
-    'tests',
-    'test_errors',
-    'test_failures',
-    'test_execution_time',
-    'security_rating'
-];
+const METRIC_LIST = ['files',
+               'functions',
+               'complexity',
+               'comment_lines_density',
+               'duplicated_lines_density',
+               'coverage',
+               'ncloc',
+               'tests',
+               'test_errors',
+               'test_failures',
+               'test_execution_time',
+               'security_rating'];
 
 const SONAR_URL = `https://sonarcloud.io/api/measures/component_tree?component=${SONAR_ID}&metricKeys=${METRIC_LIST.join(',')}&ps=500`;
 

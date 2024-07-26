faefa2e31ccda83b10f9d592a7a3ff8ebd2d2b67
Daniel Vinicius Alves <danielribeiroalves0@gmail.com>
2024-07-26 19:56:54 -0300

    feature[Input]: Adiciona componente de input.

---
+++ src/components/containers/InputContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "../../style/components/InputContainer.css";
+
+const InputContainer = ({ label, placeholder, value, onChange, className, error }) => {
+    return (
+        <div>
+            <span className="form-subtitle">{label}</span>
+            <div className="input-container">
+                <input
+                    id={label}
+                    type="text"
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={e => onChange(e.target.value)}
+                    className={error ? `input-field ${className} input-error` : `input-field ${className}`}
+                />
+            </div>
+        </div>
+    );
+};
+
+InputContainer.propTypes = {
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    className: PropTypes.string,
+    error: PropTypes.string,
+};
+
+InputContainer.defaultProps = {
+    placeholder: '',
+    error: '',
+};
+
+export default InputContainer;
+

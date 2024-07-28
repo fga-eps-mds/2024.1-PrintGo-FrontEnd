21979e0af6be3cbbc52ee1bbe5264cfc3242cd90
CAIRO <cairoflorenco@gmail.com>
2024-07-27 22:59:06 -0300

    feature{contratos}:  cria pagina de listagem de contrato
    #Refs: #5
    
    Co-authored-by: GustavoHaubert <gustavohfeitosa@outlook.com>

---
+++ src/components/containers/contractBox.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "../../style/components/contractBox.css";
+import { FaToggleOn, FaToggleOff, FaPencil } from "react-icons/fa6";
+import Button from "../Button";
+
+const ContractBox = ({ label, gestor, onEditClick, onToggleClick }) => {
+    const [isToggled, setIsToggled] = useState(true);
+
+    const handleToggleClick = () => {
+        setIsToggled(prevState => !prevState);
+        if (onToggleClick) {
+            onToggleClick();
+        }
+    };
+
+    return (
+        <div className="box">
+            <div className="overlap-group">
+                <div className="text-wrapper">
+                    <div className="numero">{label}</div>
+                    <div className="gestor">{gestor}</div>                    
+                </div>
+                <div className="actions">
+                    <div className="edit">
+                        <Button
+                            className="edit"
+                            type="icon"
+                            size="small"
+                            text={<FaPencil fontSize={'30px'} color="#0D3D6D"/>}
+                            onClick={onEditClick}
+                        />
+                    </div>
+                    <div className="toggle">
+                        <Button
+                            type="icon"
+                            size="small"
+                            text={isToggled ? <FaToggleOn fontSize={'40px'} color="#0D3D6D"/> : <FaToggleOff fontSize={'40px'} color="#0D3D6D"/>}
+                            onClick={handleToggleClick}
+                        />
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ContractBox.propTypes = {
+    label: PropTypes.string,
+    gestor: PropTypes.string,
+    onEditClick: PropTypes.func,
+    onToggleClick: PropTypes.func,
+};
+
+ContractBox.defaultProps = {
+    label: "Default Label",
+    gestor: "Default Gestor",
+    onEditClick: () => console.log('Edit button clicked'),
+    onToggleClick: () => console.log('Toggle button clicked'),
+};
+
+export default ContractBox;
--- src/components/forms/ContractListForm.js
+++ src/components/forms/ContractListForm.js
@@ -2,16 +2,50 @@ import React from "react";
 import Navbar from "../navbar/Navbar";
 import { FontSizeIcon } from "@radix-ui/react-icons";
 import ReactDOM from 'react-dom/client';
+import ContractBox from "../containers/contractBox.js";
+import "../../style/components/contractListForm.css";
 
 
-export default function ContractListForm(){
+  const mockEquipmentList = [
+    { id: 1, label: "09/2001 SSP", gestor: "João Augusto" },
+    { id: 2, label: "10/2010 SSP", gestor: "Fabio Donato" }, 
+    { id: 3, label: "15/2012 SSP", gestor: "Jennifer Loba" }, 
+    { id: 4, label: "87/2013 SSP", gestor: "Gomes Neto" }, 
+    { id: 5, label: "12/2015 SSP", gestor: "Daniel Peixe" },
+];
 
-  return (
-    <>
-    <Navbar />
-    <div>
-      <h1>Contratos Disponíveis</h1>
-    </div>
-    </>
-  );
-}
+const ContractListForm = () => {
+    const handleEditClick = (id) => {
+        console.log(`Edit button clicked for equipment ID: ${id}`);
+    };
+
+    const handleToggleClick = (id) => {
+        console.log(`Toggle button clicked for equipment ID: ${id}`);
+    };
+
+    return (
+      <>
+        <Navbar />
+        <div className="mainArea">
+          <div className="barraPesquisa">
+            <h1 className="titulo">Contratos Disponíveis</h1>
+            <input id="searchBar" placeholder="Pesquisar Contrato" type="text" maxLength={50}></input>
+          </div>
+          <div className="equipment-list">
+              {mockEquipmentList.map((equipment) => (
+                  <ContractBox
+                      key={equipment.id}
+                      label={equipment.label}
+                      gestor={equipment.gestor}
+                      onEditClick={() => handleEditClick(equipment.id)}
+                      onToggleClick={() => handleToggleClick(equipment.id)}
+                  />
+              ))}
+          </div>
+          <button id="buttonContracts">Cadastrar Novo Contrato</button>   
+        </div>
+      </>
+    );
+};
+
+export default ContractListForm;

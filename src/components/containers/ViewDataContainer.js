1e999dc6c787af9c142957811f245e73205d8b09
caiolamego <caio.lamego04@gmail.com>
2024-07-28 12:25:49 -0300

    feature[View]: desativa cursor no modo de visualização

--- src/components/containers/ViewDataContainer.js
+++ src/components/containers/ViewDataContainer.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import "../../style/components/ViewDataContainer.css";
 
 const ViewDataContainer = ({ id, name, className, labelName, value }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <div className={`view-data-container`}>
             <span className="form-subtitle">{labelName}</span>
             <label
             id={id}
             name={name}
-            className={className}
+            className={`${className} ${isHovered ? 'disabled-cursor' : ''}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
             >
                 {value}
             </label>

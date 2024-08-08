2f265f62688e1ad9cf8a130a4866b2ac2931a55f
MatheussBrant <matheus.s.brant@gmail.com>
2024-08-08 04:08:50 -0300

    feature[impressoes] - adiciona titulo
    Refs: #32

--- src/pages/AddContador.js
+++ src/pages/AddContador.js
@@ -2,29 +2,17 @@ import React from 'react';
 import '../style/pages/addcontador.css';
 import Navbar from '../components/navbar/Navbar';
 
-
-
-
-
-
 const AddContador = () => {
   return (
+    <>
+      <Navbar />
+      <div className="add-contador-header">
+        <h1>Equipamentos cadastradas</h1>
 
-    <><Navbar />
-    <div className="add-contador-container">
-          <h1>Adicionar Impressão</h1>
-          <form className="add-contador-form">
-              <label>
-                  Nome do Documento:
-                  <input type="text" name="documentName" />
-              </label>
-              <label>
-                  Quantidade de Cópias:
-                  <input type="number" name="copies" />
-              </label>
-              <button type="submit">Adicionar</button>
-          </form>
-      </div>    </>
+       
+        </div>
+      
+     </> 
   );
 };
 

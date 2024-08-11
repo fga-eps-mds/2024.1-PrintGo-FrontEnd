0a2fd130d4e7a3be2fdea405ced550b6af27e077
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2024-08-11 20:09:06 -0300

    feature: adicionando seleção de campos de localização e numero de serie
    Refs: #32

--- src/pages/AddContador.js
+++ src/pages/AddContador.js
@@ -1,25 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import '../style/pages/addcontador.css';
 import Navbar from '../components/navbar/Navbar';
+import { getPrinters, getLocalizacao } from "../services/printerService";
+import { toast } from "react-toastify";
 
 const AddContador = () => {
+  const [localizacoes, setLocalizacoes] = useState([]);
+  const [workstations, setWorkstations] = useState([]);
+  const [subworkstations, setSubworkstations] = useState([]);
   const [cidade, setCidade] = useState("");
   const [postoTrabalho, setPostoTrabalho] = useState("");
   const [subpostoTrabalho, setSubpostoTrabalho] = useState("");
-  const [equipamento, setEquipamento] = useState(""); 
+  const [equipamentos, setEquipamentos] = useState([]); 
+  const [selectedEquipamento, setSelectedEquipamento] = useState("")
   const [quantidadeImpressoes, setQuantidadeImpressoes] = useState(""); 
   const [data, setData] = useState(""); 
 
   const navigate = useNavigate(); 
+
+  useEffect(() => {
+    const fetchLocalizacoes = async () => {
+      try {
+          const response = await getLocalizacao();
+          setLocalizacoes(response.data);
+      } catch (error) {
+          console.error('Erro ao buscar localizações:', error);
+      }
+    };
+
+    const fetchContracts = async () => {
+      try {
+        const [dataEquipamentos] = await Promise.all([
+            getPrinters()
+        ]) 
+        if (dataEquipamentos.type ==='success' && dataEquipamentos.data) {
+          setEquipamentos(dataEquipamentos.data);
+          console.log(dataEquipamentos.data);
+        }
+      } catch (error) {
+        console.error('Erro ao obter lista de impressoras:', error);
+      }
+    };
+    fetchLocalizacoes();
+    fetchContracts();
+  }, []);
 
   const handleRegistrar = () => {
     navigate('/visualizarimpressora/:id');
   };
 
   const handleCancelar = () => {
-    navigate('/');
+    navigate('/impressorascadastradas');
   };
+
+  const handleLocalizacaoChange = (event) => {
+    const cidadeSelecionada = event.target.value;
+    setCidade(cidadeSelecionada);
+
+    console.log(cidade)
+
+    const localizacao = localizacoes.find(m => m.name === cidadeSelecionada);
+    setWorkstations(localizacao ? localizacao.workstations : []);
+    setSubworkstations([]);
+};
+
+const handleWorkstationChange = (event) => {
+  const workstationSelecionada = event.target.value;
+  setPostoTrabalho(workstationSelecionada);
+
+  const subworkstations = workstations.find(m => m.name === workstationSelecionada);
+  setSubworkstations(subworkstations ? subworkstations.child_workstations : []);
+};
+
+const handleSubWorkstationChange = (event) => {
+  const workstationSelecionada = event.target.value;
+  setSubpostoTrabalho(workstationSelecionada);
+};
 
   return (
     <>
@@ -34,33 +91,61 @@ const AddContador = () => {
           <div className="localizacao-fields">
             <div className="campo">
               <label>Cidade</label>
-              <select value={cidade} onChange={(e) => setCidade(e.target.value)}>
-                <option value="">Selecione </option>
-                {/* Opções que serão adicionados no Selection */}
+              <select
+                value={cidade}
+                onChange={(e) => handleLocalizacaoChange(e)}
+              >
+                <option value="">Selecione a cidade</option>
+                {localizacoes.map((localizacao) => (
+                  <option key={localizacao.id} value={localizacao.name}>
+                    {localizacao.name}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="campo">
               <label>Posto de Trabalho</label>
-              <select value={postoTrabalho} onChange={(e) => setPostoTrabalho(e.target.value)}>
-                <option value="">Selecione </option>
-                {/* Opções que serão adicionados no Selection */}
+              <select
+                value={postoTrabalho}
+                onChange={(e) => handleWorkstationChange(e)}
+              >
+                <option value="">Selecione o posto</option>
+                {workstations.map((workstation) => (
+                  <option key={workstation.id} value={workstation.id}>
+                    {workstation.name}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="campo">
               <label>Subposto de Trabalho</label>
-              <select value={subpostoTrabalho} onChange={(e) => setSubpostoTrabalho(e.target.value)}>
-                <option value="">Selecione </option>
-                {/* Opções que serão adicionados no Selection */}
+              <select
+                value={subpostoTrabalho}
+                onChange={(e) => handleSubWorkstationChange(e)}
+              >
+                <option value="">Selecione o subposto</option>
+                {subworkstations.map((subworkstation) => (
+                  <option key={subworkstation.id} value={subworkstation.name}>
+                    {subworkstation.name}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
           <div className="fields-underline"></div>
-          <div className="campo equipamento">
+          <div className="campo equipamentos">
             <label>Equipamento Associado</label>
-            <select value={equipamento} onChange={(e) => setEquipamento(e.target.value)}>
-              <option value="">Selecione </option>
-              {/* Opções que serão adicionados no Selection */}
-            </select>
+              <select
+                value={selectedEquipamento}
+                onChange={(e) => setSelectedEquipamento(e.target.value)}
+              >
+                <option value="">Número de série</option>
+                {equipamentos.map((equipamento) => (
+                  <option key={equipamento.id} value={equipamento.id}>
+                    {equipamento.numSerie}
+                  </option>
+                ))}
+              </select>
           </div>
           <div className="campo quantidade">
             <label>Quantidade de Impressões</label>

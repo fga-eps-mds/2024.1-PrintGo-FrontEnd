9f98c08abe6c3432fa09b373c5721cfe25f40a94
Marcus Escobar <marcusescobar10@gmail.com>
2024-08-13 17:35:42 -0300

    feature[Equipamentos]: Muda a cor do ItemBox do equipamento dependendo do status
    Refs: #30

--- src/components/containers/ItemBox.js
+++ src/components/containers/ItemBox.js
@@ -4,29 +4,44 @@ import "../../style/components/itemBox.css";
 import { FaToggleOn, FaToggleOff, FaPencil } from "react-icons/fa6";
 import Button from "../Button";
 import { editImpressora } from "../../services/printerService";
+import { toast } from "react-toastify";
 
 const ItemBox = ({ label, onEditClick, onSerialClick, printer }) => {
   const [isToggled, setIsToggled] = useState(printer.ativo);
 
-  const handleToggleClick = async () => {
+  const onToggleClick = async () => {
     let data = {
       ...printer,
       ativo: !printer.ativo,
       dataRetirada: new Date(),
     };
-    const response = await editImpressora(data);
-    if (response.type === "success") setIsToggled(data.ativo);
+    const res = await editImpressora(data);
+    if (res.type === "success") {
+      toast.success(
+        `Equipamento: ${printer.numSerie} ${
+          isToggled ? "desativado" : "ativado"
+        }!`
+      );
+    }
+  };
+
+  const handleToggleClick = async () => {
+    setIsToggled((prevState) => !prevState);
+    if (onToggleClick) {
+      onToggleClick();
+    }
+  };
+
+  const overlapGroupStyle = {
+    backgroundColor: isToggled ? "#ffffff" : "#d9d9d9",
   };
 
   return (
     <div className="item-box">
-      <div className="overlap-group">
+      <div className="overlap-group" style={overlapGroupStyle}>
         {/* Deixa o número de série clicável */}
         <div className="text-wrapper">
-          <span
-            onClick={onSerialClick} 
-            style={{ cursor: "pointer" }} 
-          >
+          <span onClick={onSerialClick} style={{ cursor: "pointer" }}>
             {label}
           </span>
         </div>
@@ -61,14 +76,14 @@ const ItemBox = ({ label, onEditClick, onSerialClick, printer }) => {
 ItemBox.propTypes = {
   label: PropTypes.string,
   onEditClick: PropTypes.func,
-  onSerialClick: PropTypes.func, 
+  onSerialClick: PropTypes.func,
   printer: PropTypes.object,
 };
 
 ItemBox.defaultProps = {
   label: "Default Label",
   onEditClick: () => console.log("Edit button clicked"),
-  onSerialClick: () => console.log("Serial number clicked"), 
+  onSerialClick: () => console.log("Serial number clicked"),
   printer: {},
 };
 

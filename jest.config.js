7ac9db14eda8f3d76158a22883981c087832fd23
caiolamego <caio.lamego04@gmail.com>
2024-08-15 12:13:38 -0300

    test[components]: adiciona teste para AuditBox
    Refs: #29
    
    Co-authored-by: GustavoHaubert <gustavohfeitosa@outlook.com>

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-    testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
-    testPathIgnorePatterns: ["/node_modules/"],
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest",
-      "^.+\\.svg$": "<rootDir>/fileTransform.js", // Adicione esta linha
-    },
-    moduleFileExtensions: ["js", "json", "jsx", "node", "svg"], // Adicione "svg" aqui
-    moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/src/$1",
-      "\\.(css|less|scss)$": "identity-obj-proxy",
-      "axios": "axios/dist/node/axios.cjs"
-    },
-    testEnvironment: "jsdom",
-    
-    reporters: [
-      'default',
-      [
-          'jest-sonar',
-          {
-              outputDirectory: 'reports',
-              outputName: 'sonar-report.xml'
-          }
-      ]
+  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.svg$": "<rootDir>/fileTransform.js", // Adicione esta linha
+  },
+  moduleFileExtensions: ["js", "json", "jsx", "node", "svg"], // Adicione "svg" aqui
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "axios": "axios/dist/node/axios.cjs",
+    '\\.(css|less)$': 'identity-obj-proxy', // To handle CSS imports
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.js', // Mock for static files
+  },
+  testEnvironment: "jsdom",
+
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'reports',
+        outputName: 'sonar-report.xml'
+      }
+    ]
   ]
-  };
-  
+};

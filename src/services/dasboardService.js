e483717bb471c30c3ce5f94c7f2f4b17ca372403
MatheussBrant <matheus.s.brant@gmail.com>
2024-08-29 11:48:56 -0300

    feature[dashboard]; conecta back e front
    Refs:#55

--- src/services/dasboardService.js
+++ src/services/dasboardService.js
@@ -1,20 +1,52 @@
 import { api } from '../lib/api/config';
 
 
-export const getImpressoesTotais= async () => {
-    try {
-      const response = await api.get(`/dashboard/total-impressoes`)
-      return { type: 'success', data: response.data};
-    } catch (error) {
+export const getImpressoesTotais = async (filters) => {
+  try {
+      const response = await api.get(`http://localhost:8001/dashboard/total-impressoes`, {
+          params: filters 
+      });
+      return { type: 'success', data: response.data };
+  } catch (error) {
       return { type: 'error', error };
-    }
-  };
+  }
+};
+export const getFiltroOpcoes = async () => {
+  try {
+      const response = await api.get(`http://localhost:8001/dashboard/filtro-opcoes`);
+      return { type: 'success', data: response.data };
+  } catch (error) {
+      return { type: 'error', error };
+  }
+};
 
-  export const getImpressorasColoridas= async () => {
-    try {
-      const response = await api.get(`/dashboard/color-printers`)
-      return { type: 'success', data: response.data};
-    } catch (error) {
+export const getImpressorasColoridas = async (filters) => {
+  try {
+    const response = await api.get(`http://localhost:8001/dashboard/color-printers`, {
+      params: filters // Adiciona os filtros como parâmetros na requisição
+    });
+    return { type: 'success', data: response.data };
+  } catch (error) {
+    return { type: 'error', error };
+  }
+};
+
+export const getImpressorasPB = async (filters) => {
+  try {
+    const response = await api.get(`http://localhost:8001/dashboard/pb-printers`, {
+      params: filters // Adiciona os filtros como parâmetros na requisição
+    });
+    return { type: 'success', data: response.data };
+  } catch (error) {
+    return { type: 'error', error };
+  }
+};
+
+export const getImpressionsByLocation = async () => {
+  try {
+      const response = await api.get(`http://localhost:8001/dashboard/all-printers-data`);
+      return { type: 'success', data: response.data };
+  } catch (error) {
       return { type: 'error', error };
-    }
-  };
+  }
+};

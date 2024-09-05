ac92dea8d07cd9bc6a99bdb821f1b41b36fb75dd
GustavoHaubert <gustavohfeitosa@outlook.com>
2024-09-05 14:34:58 -0300

    feature[relatorio-geral]: cria teste com 100% de cobertura para o componente GenerateExcel
    Refs: #52

--- src/components/GenerateExcel.js
+++ src/components/GenerateExcel.js
@@ -3,6 +3,48 @@ import * as XLSX from "xlsx";
 import { getPrinters } from "../services/printerService";
 import { toast } from "react-toastify";
 
+// Função que formata os dados das impressoras
+export const handleGenerateExcel = (printers) => {
+  return printers.map((printer) => ({
+    contrato: printer.numContrato,
+    numeroSerie: printer.numSerie,
+    estaNaRede: printer.estaNaRede ? "Sim" : "Não",
+    dataInstalacao: printer.dataInstalacao ? new Date(printer.dataInstalacao).toLocaleDateString() : "N/A",
+    dataRetirada: printer.dataRetirada ? new Date(printer.dataRetirada).toLocaleDateString() : "N/A",
+    dataContador: printer.dataContador ? new Date(printer.dataContador).toLocaleDateString() : "N/A",
+    ativo: printer.ativo ? "Sim" : "Não",
+    contadorInstalacaoPB: printer.contadorInstalacaoPB,
+    contadorRetiradaPB: printer.contadorRetiradaPB,
+    contadorInstalacaoCL: printer.contadorInstalacaoCor,
+    contadorRetiradaCL: printer.contadorRetiradaCor,
+    contadorPBAnterior: printer.relatorio?.contadorPB || 0,
+    contadorPBAtual: printer.contadorAtualPB,
+    contadorCLAnterior: printer.relatorio?.contadorCor || 0,
+    contadorCLAtual: printer.contadorAtualCor,
+    totPrintgoPB: printer.contadorAtualPB - printer.relatorio?.contadorPB || 0,
+    totPrintgoCL: printer.contadorAtualCor - printer.relatorio?.contadorCor || 0,
+    localizacao: printer.localizacao,
+  }));
+};
+
+// Função que gera o arquivo Excel
+export const generateExcelFile = (printers) => {
+  try {
+    const formattedPrinters = handleGenerateExcel(printers); // Usa a função para formatar os dados
+
+    const workSheet = XLSX.utils.json_to_sheet(formattedPrinters);
+    const workBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, "Relatório");
+
+    XLSX.writeFile(workBook, "Relatório.xlsx", { compression: true });
+    toast.success('Relatório gerado com sucesso!');
+  } catch (error) {
+    console.error(error);
+    toast.error('Falha ao gerar relatório excel');
+  }
+};
+
+// Componente React que chama as funções
 export default function GenerateExcel() {
   const [printers, setPrinters] = useState([]);
 
@@ -11,95 +53,9 @@ export default function GenerateExcel() {
     setPrinters(res.data);
   };
 
-  const handleGenerateExcel = () => {
-    try {
-      const formattedPrinters = printers.map((printer) => {
-        return {
-          contrato: printer.numContrato,
-          numeroSerie: printer.numSerie,
-          estaNaRede: printer.estaNaRede ? "Sim" : "Não",
-          dataInstalacao: new Date(printer.dataInstalacao).toLocaleDateString(),
-          dataRetirada: printer.dataRetirada ? new Date(printer.dataRetirada).toLocaleDateString() : "N/A",
-          dataContador: printer.dataContador ? new Date(printer.dataContador).toLocaleDateString() : "N/A",
-          ativo: printer.ativo ? "Sim" : "Não",
-          contadorInstalacaoPB: printer.contadorInstalacaoPB,
-          contadorRetiradaPB: printer.contadorRetiradaPB,
-          contadorInstalacaoCL: printer.contadorInstalacaoCor,
-          contadorRetiradaCL: printer.contadorRetiradaCor,
-          contadorPBAnterior: printer.relatorio?.contadorPB || 0,
-          contadorPBAtual: printer.contadorAtualPB,
-          contadorCLAnterior: printer.relatorio?.contadorCor || 0,
-          contadorCLAtual: printer.contadorAtualCor,
-          totPrintgoPB: printer.contadorAtualPB - printer.relatorio?.contadorPB || 0,
-          totPrintgoCL: printer.contadorAtualCor - printer.relatorio?.contadorCor || 0,
-          localizacao: printer.localizacao,
-        };
-      });
-
-      console.log(formattedPrinters);
-      const workSheet = XLSX.utils.json_to_sheet(formattedPrinters);
-      const workBook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workBook, workSheet, "Relatório");
-
-      XLSX.utils.sheet_add_aoa(
-        workSheet,
-        [
-          [
-            "Contrato",
-            "Nº de Serie",
-            "Na Rede",
-            "Data Instalação",
-            "Data Retirada",
-            "Data Contador",
-            "Ativo",
-            "Contador Instalacao PB",
-            "Contador Retirada PB",
-            "Contador Instalacao CL",
-            "Contador Retirada CL",
-            "Contador PB Anterior",
-            "Contador PB Atual",
-            "Contador CL Anterior",
-            "Contador CL Atual",
-            "Total PrintGo PB",
-            "Total PrintGo CL",
-            "Localização",
-          ],
-        ],
-        { origin: "A1" }
-      );
-
-      workSheet["!cols"] = [  //Tamanho das colunas
-        { wch: 15 },          // Contrato
-        { wch: 20 },          // Nº de Serie
-        { wch: 10 },          // Na Rede
-        { wch: 15 },          // Data Instalação
-        { wch: 15 },          // Data Retirada
-        { wch: 15 },          // Data Contador
-        { wch: 10 },          // Ativo
-        { wch: 20 },          // Contador Instalacao PB
-        { wch: 20 },          // Contador Retirada PB
-        { wch: 20 },          // Contador Instalacao CL
-        { wch: 20 },          // Contador Retirada CL
-        { wch: 20 },          // Contador PB Anterior
-        { wch: 20 },          // Contador PB Atual
-        { wch: 20 },          // Contador CL Anterior
-        { wch: 20 },          // Contador CL Atual
-        { wch: 20 },          // Total PrintGo PB
-        { wch: 20 },          // Total PrintGo CL
-        { wch: 25 },          // Localização
-      ];
-
-      XLSX.writeFile(workBook, "Relatório.xlsx", { compression: true });
-      toast.success('Relatório gerado com sucesso!');
-
-    } catch (error) {
-      console.error(error);
-      toast.error('Falha ao gerar relatório excel');
-    }
-  };
-
   useEffect(() => {
     fetchPrinters();
   }, []);
-  return <button onClick={handleGenerateExcel}>Gerar Relatório Geral</button>;
+
+  return <button onClick={() => generateExcelFile(printers)}>Gerar Relatório Geral</button>;
 }
--- src/tests/components/GenerateExcel.test.js
+++ src/tests/components/GenerateExcel.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import GenerateExcel from '../../components/GenerateExcel';
+import GenerateExcel, { handleGenerateExcel, generateExcelFile } from '../../components/GenerateExcel';
 import { getPrinters } from '../../services/printerService';
 import * as XLSX from 'xlsx';
 import { toast } from 'react-toastify';
@@ -22,32 +22,143 @@ jest.mock('xlsx', () => ({
 
 describe("GenerateExcel Component", () => {
     beforeEach(() => {
-        getPrinters.mockResolvedValue({
+       getPrinters.mockResolvedValue({
             type: 'success',
             data: [
                 {
                     id: 1,
-                    numContrato: "123",
-                    numSerie: "ABC",
-                    enderecoIp: "192.168.0.1",
+                    numContrato: "12345",
+                    numSerie: "SN123456789",
+                    enderecoIp: "192.168.1.100",
                     estaNaRede: true,
-                    dataInstalacao: "2023-08-01T00:00:00.000Z",
-                    dataRetirada: "2023-09-01T00:00:00.000Z",
-                    dataContador: "2023-08-01T00:00:00.000Z",
+                    dataInstalacao: "2023-01-01T00:00:00Z",
+                    dataRetirada: "2023-06-01T00:00:00Z",
+                    dataContador: "2023-09-01T00:00:00Z",
                     ativo: true,
-                    contadorInstalacaoPB: 100,
-                    contadorInstalacaoCor: 200,
-                    contadorAtualPB: 150,
-                    contadorAtualCor: 250,
-                    contadorRetiradaPB: 200,
-                    contadorRetiradaCor: 300,
-                    localizacao: "Goiânia",
-                    modeloId: "2x24",
-                },
-            ],
+                    contadorInstalacaoPB: 5000,
+                    contadorInstalacaoCor: 1000,
+                    contadorAtualPB: 7500,
+                    contadorAtualCor: 1600,
+                    contadorRetiradaPB: 7000,
+                    contadorRetiradaCor: 1500,
+                    localizacao: "Escritório Central",
+                    modeloId: "MX500",
+                    relatorio: {
+                      id: 1,
+                      impressoraId: 1,
+                      contadorPB: 4000,
+                      contadorPBDiff: 1500,
+                      contadorCor: 800,
+                      contadorCorDiff: 800,
+                      ultimoResultado: 500,
+                      resultadoAtual: 600,
+                      ultimaAtualizacao: "2023-08-01T00:00:00Z"
+                    },
+                    relatorioLocadora: {
+                      id: 1,
+                      impressoraId: 1,
+                      contadorPB: 4500,
+                      contadorCor: 900,
+                      contadorTotal: 5400
+                    }
+                }
+            ], 
         });
         toast.error = jest.fn();
         toast.success = jest.fn();
+    });
+
+    test("should call handleGenerateExcel correctly", () => {
+        const mockPrinters = [
+            {
+                numContrato: "123",
+                numSerie: "ABC",
+                estaNaRede: true,
+                dataInstalacao: "2023-08-10T00:00:00.000Z",
+                dataRetirada: "2023-09-10T00:00:00.000Z",
+                dataContador: "2023-08-10T00:00:00.000Z",
+                ativo: true,
+                contadorInstalacaoPB: 100,
+                contadorInstalacaoCor: 200,
+                contadorAtualPB: 150,
+                contadorAtualCor: 250,
+                contadorRetiradaPB: 200,
+                contadorRetiradaCor: 300,
+                localizacao: "Goiânia",
+                relatorio: {
+                  contadorPB: 50,
+                  contadorCor: 100,
+                },
+            },
+        ];
+
+        const formatted = handleGenerateExcel(mockPrinters);
+
+        expect(formatted).toEqual([
+          {
+            contrato: "123",
+            numeroSerie: "ABC",
+            estaNaRede: "Sim",
+            dataInstalacao: "09/08/2023",
+            dataRetirada: "09/09/2023",
+            dataContador: "09/08/2023",
+            ativo: "Sim",
+            contadorInstalacaoPB: 100,
+            contadorRetiradaPB: 200,
+            contadorInstalacaoCL: 200,
+            contadorRetiradaCL: 300,
+            contadorPBAnterior: 50,
+            contadorPBAtual: 150,
+            contadorCLAnterior: 100,
+            contadorCLAtual: 250,
+            totPrintgoPB: 100,
+            totPrintgoCL: 150,
+            localizacao: "Goiânia",
+          }
+        ]);
+    });
+
+    test("should call handleGenerateExcel corretly with other options", () => {
+        const mockPrinters = [
+            {
+                numContrato: "123",
+                numSerie: "ABC",
+                estaNaRede: false,
+                ativo: false,
+                contadorInstalacaoPB: 100,
+                contadorInstalacaoCor: 200,
+                contadorAtualPB: 150,
+                contadorAtualCor: 250,
+                contadorRetiradaPB: 200,
+                contadorRetiradaCor: 300,
+                localizacao: "Goiânia",
+            },
+        ];
+
+        const formatted = handleGenerateExcel(mockPrinters);
+
+        expect(formatted).toEqual([
+          {
+            contrato: "123",
+            numeroSerie: "ABC",
+            estaNaRede: "Não",
+            dataInstalacao: "N/A",
+            dataRetirada: "N/A",
+            dataContador: "N/A",
+            ativo: "Não",
+            contadorInstalacaoPB: 100,
+            contadorRetiradaPB: 200,
+            contadorInstalacaoCL: 200,
+            contadorRetiradaCL: 300,
+            contadorPBAnterior: 0,
+            contadorPBAtual: 150,
+            contadorCLAnterior: 0,
+            contadorCLAtual: 250,
+            totPrintgoPB: 0,
+            totPrintgoCL: 0,
+            localizacao: "Goiânia",
+          }
+        ]);
     });
 
     test("renders GenerateExcel component and button", async () => {
@@ -57,34 +168,36 @@ describe("GenerateExcel Component", () => {
         expect(screen.getByText("Gerar Relatório Geral")).toBeInTheDocument();
     });
 
-    test('should generate an Excel file when button is clicked', async () => {
-    
-        // Render the component
-        render(<GenerateExcel />);
-    
-        // Wait for printers to be fetched
-        await waitFor(() => expect(getPrinters).toHaveBeenCalledTimes(2));
-    
-        // Trigger the Excel generation by clicking the button
+    test('should generate Excel correctly', async () => {
+        render(<GenerateExcel/>);
+        await waitFor(() => expect(screen.getByText('Gerar Relatório Geral')).toBeInTheDocument());
         fireEvent.click(screen.getByText('Gerar Relatório Geral'));
-    
-        // Check if XLSX functions were called correctly
-        expect(XLSX.utils.json_to_sheet).toHaveBeenCalled();
-        expect(XLSX.utils.book_new).toHaveBeenCalled();
-        expect(XLSX.utils.book_append_sheet).toHaveBeenCalled();
-        expect(XLSX.writeFile).toHaveBeenCalled();
-    
-        // Check if success toast was called
+        await waitFor(() => {
+            expect(XLSX.utils.json_to_sheet).toHaveBeenCalled();
+            expect(XLSX.utils.book_new).toHaveBeenCalled();
+            expect(XLSX.utils.book_append_sheet).toHaveBeenCalled();
+            expect(XLSX.writeFile).toHaveBeenCalled();
+          });
         expect(toast.success).toHaveBeenCalledWith('Relatório gerado com sucesso!');
     });
 
-    test('should process printers data', async () => {
-        render(<GenerateExcel />);
+    test('should throw error mesage', async () => {
+        // Simula um erro na função XLSX.writeFile
+        XLSX.writeFile.mockImplementation(() => {
+          throw new Error('Erro ao gerar Excel');
+        });
+    
+        render(<GenerateExcel />); // Renderiza o componente
         
-        await waitFor(() => expect(getPrinters).toHaveBeenCalled());
-
-        expect(getPrinters).not.toHaveProperty('id');
-        expect(getPrinters).not.toHaveProperty('enderecoIp');
-        expect(getPrinters).not.toHaveProperty('modeloId');
-    });
+        // Aguarda os dados serem carregados
+        await waitFor(() => expect(screen.getByText('Gerar Relatório Geral')).toBeInTheDocument());
+    
+        // Simula o clique no botão para gerar o relatório
+        fireEvent.click(screen.getByText('Gerar Relatório Geral'));
+    
+        // Verifica se o toast de erro foi chamado
+        await waitFor(() => {
+          expect(toast.error).toHaveBeenCalledWith('Falha ao gerar relatório excel');
+        });
+      });
 });
